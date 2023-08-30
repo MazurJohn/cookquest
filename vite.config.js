466be3eb00dabe0e2cabb3d124b568a398f69944
MazurJohn <forprogwork@gmail.com>
2023-08-30 18:33:34 +0300

    First

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://MazurJohn.github.io/cookquest",
+  base: "https://mazurjohn.github.io/cookquest",
   plugins: [react()],
 });
