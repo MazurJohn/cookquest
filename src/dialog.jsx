2f7df1e2237a19e06b1c5c2a0ac2623a1eeb8dcb
MazurJohn <forprogwork@gmail.com>
2023-09-07 16:32:22 +0300

    alert dialog

--- src/dialog.jsx
+++ src/dialog.jsx
@@ -5,31 +5,47 @@ import {
   DialogHeader,
   DialogBody,
   DialogFooter,
+  Alert,
 } from "@material-tailwind/react";
 
 function AlertDialog({ open, onClose, title, content, onConfirm }) {
+  const [openAlert, setOpenAlert] = React.useState(false);
   const handleConfirm = () => {
     onConfirm();
     onClose();
+    setOpenAlert(true);
   };
 
   return (
-    <Dialog
-      open={open}
-      handler={onClose}
-      className="flex flex-col items-center"
-    >
-      <DialogHeader>{title}</DialogHeader>
-      <DialogBody divider>{content}</DialogBody>
-      <DialogFooter>
-        <Button variant="text" color="red" onClick={onClose} className="mr-1">
-          <span>Відмінити</span>
-        </Button>
-        <Button variant="gradient" color="green" onClick={handleConfirm}>
-          <span>Підтвердити</span>
-        </Button>
-      </DialogFooter>
-    </Dialog>
+    <>
+      <Dialog
+        open={open}
+        handler={onClose}
+        className="flex flex-col items-center"
+      >
+        <DialogHeader>{title}</DialogHeader>
+        <DialogBody divider>{content}</DialogBody>
+        <DialogFooter>
+          <Button variant="text" color="red" onClick={onClose} className="mr-1">
+            <span>Відмінити</span>
+          </Button>
+          <Button variant="gradient" color="green" onClick={handleConfirm}>
+            <span>Підтвердити</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
+      <Alert
+        color="green"
+        open={openAlert}
+        onClose={() => setOpenAlert(false)}
+        animate={{
+          mount: { y: 0 },
+          unmount: { y: 100 },
+        }}
+      >
+        Успішно!
+      </Alert>
+    </>
   );
 }
 
